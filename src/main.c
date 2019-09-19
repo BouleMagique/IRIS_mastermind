9d8fdb997e391d48bb006f4e5aa984c8d09268e5
Dimitri <dimrichard404@gmail.com>
2019-09-19 17:56:44 +0200

    added a bunch of functions

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+Title: main.c
+desc: main fil efor mastermind
+Projetc: IRIS_mastermind
+author: Dimitri
+version: 0.1
+ **/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int  main() {
+  system("./display.sh");
+  //menu avec regles, un joli affichage ect...
+  return(0);
+}
+	   
---
+++ src/mastermind.c
@@ -0,0 +1,5 @@
+int mastermind(/*on verra plus tard si ya des arg ici*/) {
+  //fonction principale
+
+  return(0);
+}
